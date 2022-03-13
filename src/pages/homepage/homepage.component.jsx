2bf190ce28e5425295a1f4231e209d51bfdc08a1
nabilfatih <nabilakbarazzima@gmail.com>
2022-03-13 16:15:16 +0100

    add router

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import Directory from "../../components/directory/directory.component";
-import './homepage.styles.scss'
+import React from 'react';
 
-const HomePage = () => {
-    return (
-        <div className="homepage ">
-            <Directory />
-        </div>
-    )
-}
+import Directory from '../../components/directory/directory.component';
 
-export default HomePage
+import './homepage.styles.scss';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <Directory />
+  </div>
+);
+
+export default HomePage;
